5fd3e5e3619772eaa8b322db3eedcafc65737080
Nikunj <nikunj.agola@tatvasoft.com>
2020-03-11 11:08:53 +0530

    Update AppDelegate.swift
    Commit 2

--- Movie_App/Movie_App/Application/AppDelegate.swift
+++ Movie_App/Movie_App/Application/AppDelegate.swift
@@ -34,6 +34,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
 //        Commit 1
+//        Commit 2
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = navigationController
