2f04d5fb1d18a5c30698f72806a94829a4c4480c
Nikunj <nikunj.agola@tatvasoft.com>
2020-03-11 11:08:20 +0530

    Update AppDelegate.swift
    Commit 1

--- Movie_App/Movie_App/Application/AppDelegate.swift
+++ Movie_App/Movie_App/Application/AppDelegate.swift
@@ -33,6 +33,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+//        Commit 1
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = navigationController
         
